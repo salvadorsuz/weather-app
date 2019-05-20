7194e79dcd3424313167834f52b1f32cc0f7354d
salvadorsuz <salvadorsuz@yahoo.es>
2019-05-20 10:45:55 +0200

    reducers

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import LocationList from './components/LocationList';
 import { Grid, Row, Col } from 'react-flexbox-grid';
 import Paper from '@material-ui/core/Paper';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import ForecastExtended from './components/ForecastExtended';
-import { setCity } from './actions';
-import { store } from './store';
+import LocationListContainer from './containers/LocationListContainer';
+import ForecastExtendedContainer from './containers/ForecastExtendedContainer';
 import './App.css';
 
 const cities = [
@@ -20,20 +18,7 @@ const cities = [
 
 class App extends Component {
 
-  constructor() {
-    super();
-    this.state = {city: null};
-  }
-
-  handleSelectionLocation = city => {
-    this.setState({city});
-    console.log(`handleSelectionLocation ${city}`);
-
-    store.dispatch(setCity(city));
-  }
-
   render() {
-    const { city } = this.state;
     return (
       <Grid>
         <Row>
@@ -47,14 +32,12 @@ class App extends Component {
         </Row>
         <Row>
           <Col xs={12} md={6}>
-            <LocationList cities={cities} onSelectedLocation={this.handleSelectionLocation} />
+            <LocationListContainer cities={cities} />
           </Col>
           <Col xs={12} md={6}>
             <Paper elevation={4}>
               <div className="details">
-                {
-                  city && <ForecastExtended city={city}></ForecastExtended> 
-                }
+                  <ForecastExtendedContainer />
               </div>
             </Paper>
           </Col>
