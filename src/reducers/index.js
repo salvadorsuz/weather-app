97984bc9ce5773b5212f70f02606fa11a140212d
salvadorsuz <salvadorsuz@yahoo.es>
2019-05-29 19:34:45 +0200

    refactor getCities selector

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -12,6 +12,8 @@ export default combineReducers({
 
 export const getCity = createSelector(state => state.city, city => city);
 
-export const getForecastDataFromCities = createSelector(state => state.cities,  getCity, _getForecastDataFromCities);
+const getCities = createSelector(state => state.cities, cities => cities);
 
-export const getWeatherCities = createSelector( state => state.cities, _getWeatherCities);
+export const getForecastDataFromCities = createSelector(getCities,  getCity, _getForecastDataFromCities);
+
+export const getWeatherCities = createSelector(getCities, _getWeatherCities);
